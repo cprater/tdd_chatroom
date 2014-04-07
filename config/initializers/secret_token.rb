35b6b39742e769164a4eef53117be71779b85f2a
Chris Prater <cprater23@gmail.com>
2014-04-07 13:25:18 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chatroom::Application.config.secret_key_base = '0058317202c47e88072e327cab42c24128a38368901e6b8cf9162432fe4669512dc24e729d47a4ac4c187d3085c1447eb62c6ad4f987f39539c89293c1f723c9'
