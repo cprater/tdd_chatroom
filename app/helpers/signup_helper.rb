69af3c494d825c348237075f3a405e36760b57c9
Chris Prater <cprater23@gmail.com>
2014-04-11 13:12:41 -0500

    finished(I'm pretty sure) User testing

--- app/helpers/signup_helper.rb
+++ app/helpers/signup_helper.rb
@@ -1,8 +1,11 @@
 module SignupHelper
-	def determine_signup_error(params)
-		# binding.pry
+	def determine_signup_error(params)		
 		if User.find_by_username(params[:username]) || params[:username].empty?
 			return "Invalid Username"
+		elsif params[:password].empty?
+			return "Enter a Password"
+		elsif params[:password] != params[:password_confirmation]
+			return "Passwords don't match"
 		end
 
 	end
--- spec/features/user_features_spec.rb
+++ spec/features/user_features_spec.rb
@@ -19,25 +19,39 @@ describe UserController do
 			expect(page).to have_content('Welcome test')
 		end
 
-		it 'render correct error message when no username entered' do 
-			visit root_path
-			within('#new_user') do 
-				
-				fill_in 'user[password]', :with => '123'
-				fill_in 'user[password_confirmation]', :with => '123'
-				click_button 'Create User'
-			end
-			expect(page).to have_content('Invalid Username')
-		end
+		context 'render correct error message when ' do 
 
-		it 'render correct error message when no password entered' do 
-			visit root_path
-			within('#new_user') do 
-				fill_in 'user[username]', :with => 'test'
-				
-				click_button 'Create User'
+			it 'no username entered' do 
+				visit root_path
+				within('#new_user') do 
+					
+					fill_in 'user[password]', :with => '123'
+					fill_in 'user[password_confirmation]', :with => '123'
+					click_button 'Create User'
+				end
+				expect(page).to have_content('Invalid Username')
 			end
-			expect(page).to have_content('Enter a Password')
+
+			it 'no password entered' do 
+				visit root_path
+				within('#new_user') do 
+					fill_in 'user[username]', :with => 'test'
+					
+					click_button 'Create User'
+				end
+				expect(page).to have_content('Enter a Password')
+			end
+
+			it 'passwords don\'t match' do 
+				visit root_path
+				within('#new_user') do 
+					fill_in 'user[username]', :with => 'test'
+					fill_in 'user[password]', :with => '222'
+					fill_in 'user[password_confirmation]', :with => '123'
+					click_button 'Create User'
+				end
+				expect(page).to have_content('Passwords don\'t match')
+			end
 		end
 	end
 
