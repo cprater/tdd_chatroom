e34d8964003841fa3087f0e7be606585951fb95d
Chris Prater <cprater23@gmail.com>
2014-04-14 17:04:52 -0500

    members names only appear once in rooms view now

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -3,20 +3,18 @@ class RoomController < ApplicationController
 
 	def create
 		@room = Room.create(name: params[:chatroom][:name], created_by: session[:current_user])
+		@room.join(User.find(session[:current_user]))
 		redirect_to room_path(@room.id)
 	end
 
 	def show
 		@room = Room.find(params[:id])
-		@room.join(User.find(session[:current_user]))
 	end
 
 	def add_message
 		username = User.find(session[:current_user]).username
 		@room = Room.find(params[:sending][:room_id])
-		# binding.pry
 		message = Message.new(message: params[:sending][:message], username: username)
-
 		@room.messages << message
 		@room.save
 		redirect_to room_path(@room.id)
